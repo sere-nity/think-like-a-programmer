55522ea0147602ac7a4e47acbc5ddb7f350d8a0e
Serene Liu <serene6197@gmail.com>
2022-08-08 10:31:54 +0100

    Complete Ex 2.2 in 2.1

--- chapter-2/Ex-2.1-hashShapes.cpp
+++ chapter-2/Ex-2.1-hashShapes.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(){
     for (int row = 1; row <= 4; row++){
+        for (int space = 1; space <= 4-row; space++){
+            cout << " ";
+        }
+        for (int hashNum = 1; hashNum <= row*2; hashNum++){
+            cout << "#";
+        }
+        cout << "\n";
+    }
+
+    for (int row = 1; row <= 4; row++){
         for (int space = 1; space <= row - 1; space++){
             cout << " ";
         }
